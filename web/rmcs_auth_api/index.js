f9603f3eeb096c68bf9c25d99ca0c4ff298fe303
Chandra Wijaya Sentosa <chandra.w.sentosa@gmail.com>
2024-06-13 15:51:11 +0700

    Bundle grpc-web generated files

---
+++ web/rmcs_auth_api/index.js
@@ -0,0 +1,7 @@
+import pb_api from './api_grpc_web_pb.js';
+import pb_role from './role_grpc_web_pb.js';
+import pb_user from './user_grpc_web_pb.js';
+import pb_token from './token_grpc_web_pb.js';
+import pb_auth from './auth_grpc_web_pb.js';
+
+export { pb_api, pb_role, pb_user, pb_token, pb_auth };
