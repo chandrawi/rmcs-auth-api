2278c33eda8d585fb196a93884b6a7e41b9c9bfe
Chandra Wijaya Sentosa <chandra.w.sentosa@gmail.com>
2025-07-03 14:14:54 +0700

    Update dependency

--- python/src/rmcs_auth_api/role_pb2_grpc.py
+++ python/src/rmcs_auth_api/role_pb2_grpc.py
@@ -5,7 +5,7 @@ import warnings
 
 from rmcs_auth_api import role_pb2 as rmcs__auth__api_dot_role__pb2
 
-GRPC_GENERATED_VERSION = '1.71.0'
+GRPC_GENERATED_VERSION = '1.73.1'
 GRPC_VERSION = grpc.__version__
 _version_not_supported = False
 
--- python/src/rmcs_auth_api/token_pb2_grpc.py
+++ python/src/rmcs_auth_api/token_pb2_grpc.py
@@ -5,7 +5,7 @@ import warnings
 
 from rmcs_auth_api import token_pb2 as rmcs__auth__api_dot_token__pb2
 
-GRPC_GENERATED_VERSION = '1.71.0'
+GRPC_GENERATED_VERSION = '1.73.1'
 GRPC_VERSION = grpc.__version__
 _version_not_supported = False
 
